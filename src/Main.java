6ae1b74f4da88f72bbcdf4c1092541a7ecca366f
VGE <estebitanvaras@gmail.com>
2025-11-06 19:34:41 +0100

    Añadiendo texto

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hola mi gente estamo en japon");
+        System.out.println("La resta 2-1 = 1");
     }
 }
